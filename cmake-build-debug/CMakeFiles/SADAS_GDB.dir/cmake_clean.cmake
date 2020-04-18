9e198983295cbf7c6bd2d3c2129fe2ce9dbeec9d
magemma <martini.gemma3@gmail.com>
2020-04-18 11:19:54 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/SADAS_GDB.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SADAS_GDB.dir/main.cpp.o"
+  "SADAS_GDB"
+  "SADAS_GDB.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SADAS_GDB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/SADAS_GDB.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for SADAS_GDB.
+# This may be replaced when dependencies are built.
